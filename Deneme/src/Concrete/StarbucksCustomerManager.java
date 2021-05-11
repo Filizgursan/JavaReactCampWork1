0aa3002b0315c64643fdb0daf804ba3acba4dcb5
filizzgursan <filizzgursan@outlook.com>
2021-05-12 00:34:30 +0300

    CoffeeProject

---
+++ Deneme/src/Concrete/StarbucksCustomerManager.java
@@ -0,0 +1,24 @@
+package Concrete;
+
+import Abstract.BaseCustomerManager;
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+
+public class StarbucksCustomerManager extends BaseCustomerManager{
+	private CustomerCheckService customerCheckService;
+	
+	public StarbucksCustomerManager(CustomerCheckService customerCheckService) {
+		
+		this.customerCheckService = customerCheckService;
+	}
+	
+	public void save(Customer customer) {
+		if(customerCheckService.CheckIfRealPerson(customer) == true) {
+			System.out.println("Save to db : " + customer.getFirstName());
+		}
+		else {
+			System.out.println("Not a valid person");
+		}
+	}
+
+}
---
+++ Deneme/src/Main.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		Customer customer = new Customer();
+		customer.setId(1);
+		customer.setFirstName("Filiz");
+		customer.setLastName("Gürsan");
+		customer.setDateOfBirth(LocalDate.of(1999,10,19));
+		customer.setNationalityId("1234567");
+		
+		baseCustomerManager.save(customer);
+		
+		try { 
+			
+		}
+		catch(Exception e1) 
+		{     
+			e1.getMessage();
+	    }
+
+	}
+
+}
